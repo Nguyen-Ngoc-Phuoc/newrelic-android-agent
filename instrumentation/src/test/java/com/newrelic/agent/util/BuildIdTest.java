63365af9e2a1df5bc00a04d6ecbbcba30efb41ac
Nguyen, Phuoc <phuoc.nguyen2@hrs.com>
2024-10-30 17:54:37 +0700

    change cond

--- instrumentation/src/test/java/com/newrelic/agent/util/BuildIdTest.java
+++ instrumentation/src/test/java/com/newrelic/agent/util/BuildIdTest.java
@@ -48,6 +48,16 @@ public class BuildIdTest {
     }
 
     @Test
+    public void getCustomBuildIdVariantMapEnabled() {
+        String customBuildId = "test build id";
+        BuildId.setCustomBuildId(customBuildId);
+        String buildId = BuildId.getBuildId("variant");
+        Assert.assertNotNull(buildId);
+        Assert.assertNotEquals(buildId, customBuildId);
+    }
+
+
+    @Test
     public void getBuildId() {
         String buildId = BuildId.getBuildId("test");
         Assert.assertNotNull(buildId);
@@ -86,4 +96,20 @@ public class BuildIdTest {
         variantBuildId = BuildId.getBuildId("variant");
         Assert.assertEquals(buildId, variantBuildId);
     }
+
+    @Test
+    public void getCustomBuildIdVariantMapDisabled() {
+
+        BuildId.setVariantMapsEnabled(false);
+        String customBuildId = "test build id";
+        BuildId.setCustomBuildId(customBuildId);
+
+        BuildId.invalidate();
+
+        String buildId = BuildId.getDefaultBuildId();
+        String variantBuildId = BuildId.getBuildId("variant");
+        Assert.assertEquals(buildId, variantBuildId);
+        Assert.assertEquals(buildId, customBuildId);
+    }
+
 }
